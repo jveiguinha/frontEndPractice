ace1e4e4833ca1014b1135696d733bf74bdf5c7f
jveiguinha <j.veiguinha01@gmail.com>
2024-04-24 19:21:32 +0100

    Abletone v.0.1.3

--- Ableton/ableton.js
+++ Ableton/ableton.js
@@ -1,4 +1,5 @@
 function hiddenThumb() {
     document.getElementById("abletonVideoThumbnail").className = "hidden";
     document.getElementById("videoPlayButton").className = "hidden";
+    document.getElementById("videoId").src = 'https://www.youtube.com/embed/9SbnhgjeyXA?si=Bo5kzJf7R4UJBbEU&autoplay=1';
 }
